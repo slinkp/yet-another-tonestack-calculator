4cfb544687226dfd5289542a0e53fc72a7518317
Paul Winkler <slinkp@gmail.com>
2025-10-23 00:58:32 -0400

    SK lowpass - better defaults
    Min frequency is about 100Hz, midpoint is about 350Hz, max is still >100Hz.
    
    Added some comments re things I'd like to fix if i can.

--- resources/lcapy/circuits/Basic/SallenKeyLowpass.sch
+++ resources/lcapy/circuits/Basic/SallenKeyLowpass.sch
@@ -1,13 +1,27 @@
 ---
 id: skl0
 name: Sallen–Key Low-pass (Butterworth, unity)
-controls: {}
+controls:
+  RFREQ1: LogC
+  RFREQ2: LogC
+
+description: RFREQ1 and RFREQ2 are typically one dual-ganged pot. For butterworth response (12dB/octave and no resonant peak), set C1 to twice C2. For higher Q, increase that ratio.
+
 ---
-RIN IN N1 10000; right
-R2 N1 N_OANONINV 10000; right
-C2 N_OANONINV 0 10e-9; down
+
+;; I would make a dual-ganged pot here if I knew how
+RFREQ1 IN N1 500e3; right, variable, *
+RFREQ2 N1 N_OANONINV 500e3; right, variable, *
+
+;; TODO add two fixed resistors here to set the max cutoff freq
+;; (one before RFREQ1, the other before RFREQ2).
+;; When the pots are 0 we get max and it's always uselessly flat to 100k.
+;; Decent starting points = 3k?
+
+;; For butterworth set C1 = 2*C2
+C1 N3 N4 43e-10; right
+C2 N_OANONINV 0 22e-10; down
 W1 N1 N3; up
-C1 N3 N4 20e-9; right
 
 ;; For debug only
 ;A4 N4; l_=N4, yoffset=0.5
@@ -29,7 +43,6 @@ W4 N5 OUT; down
 ;; i'm using "typical" value from TL071 data sheet of 118dB = approx 630,957
 E OUT 0 opamp N_OANONINV N_OAINV Ro=1e-10 Ac=1e-10 Ad=631000; right, mirror, scale=0.5
 
-;; Feedback time. Does a resistor fix the math domain error? No it does not. Flailing here.
 WFEEDBACK N4 N5 ; right
 
 RL OUT 0 10e5; down
--- src/models/tonestacks/Basic/SallenKeyLowpass.js
+++ src/models/tonestacks/Basic/SallenKeyLowpass.js
@@ -6,32 +6,43 @@ export class SallenKeyLowpass extends BaseTonestack {
     return {
       id: 'skl0',
       name: 'Sallen–Key Low-pass (Butterworth, unity)',
+      description: 'RFREQ1 and RFREQ2 are typically one dual-ganged pot. For butterworth response (12dB/octave and no resonant peak), set C1 to twice C2. For higher Q, increase that ratio.',
       schematic: 'SallenKeyLowpass',
       components: {
-        RIN: 10e3,
-        R2: 10e3,
+        RFREQ1: 500e3,
+        RFREQ2: 500e3,
         RL: 1e6,
-        C1: 20e-9,
-        C2: 10e-9,
+        C1: 4.3e-9,
+        C2: 2.2e-9,
         E: 631e3,
         E_Ac: 100e-12,
         E_Ro: 100e-12,
       },
       controls: {
+        RFREQ1: {
+          taper: Tapers.LogC,
+          role: PotRole.VR,
+          reverse: true,
+        },
+        RFREQ2: {
+          taper: Tapers.LogC,
+          role: PotRole.VR,
+          reverse: true,
+        },
       }
     };
   }
 
   calculateCoefficients(controlValues) {
-    const { RIN, R2, RL, C1, C2, E, E_Ac, E_Ro } = this.extractCoefficientVariables(controlValues);
+    const { RFREQ1, RFREQ2, RL, C1, C2, E, E_Ac, E_Ro } = this.extractCoefficientVariables(controlValues);
 
     const b0 = 2*E*RL + E_Ac*RL;
     const b1 = 2*C1*E_Ro*RL;
-    const b2 = 2*C1*C2*E_Ro*R2*RL;
+    const b2 = 2*C1*C2*E_Ro*RFREQ2*RL;
 
     const a0 = 2*E*RL - E_Ac*RL + 2*E_Ro + 2*RL;
-    const a1 = -2*C1*E_Ac*RIN*RL + 2*C1*E_Ro*RIN + 2*C1*E_Ro*RL + 2*C1*RIN*RL + 2*C2*E*R2*RL + 2*C2*E*RIN*RL - C2*E_Ac*R2*RL - C2*E_Ac*RIN*RL + 2*C2*E_Ro*R2 + 2*C2*E_Ro*RIN + 2*C2*R2*RL + 2*C2*RIN*RL;
-    const a2 = 2*C1*C2*E*R2*RIN*RL - C1*C2*E_Ac*R2*RIN*RL + 2*C1*C2*E_Ro*R2*RIN + 2*C1*C2*E_Ro*R2*RL + 2*C1*C2*E_Ro*RIN*RL + 2*C1*C2*R2*RIN*RL;
+    const a1 = -2*C1*E_Ac*RFREQ1*RL + 2*C1*E_Ro*RFREQ1 + 2*C1*E_Ro*RL + 2*C1*RFREQ1*RL + 2*C2*E*RFREQ1*RL + 2*C2*E*RFREQ2*RL - C2*E_Ac*RFREQ1*RL - C2*E_Ac*RFREQ2*RL + 2*C2*E_Ro*RFREQ1 + 2*C2*E_Ro*RFREQ2 + 2*C2*RFREQ1*RL + 2*C2*RFREQ2*RL;
+    const a2 = 2*C1*C2*E*RFREQ1*RFREQ2*RL - C1*C2*E_Ac*RFREQ1*RFREQ2*RL + 2*C1*C2*E_Ro*RFREQ1*RFREQ2 + 2*C1*C2*E_Ro*RFREQ1*RL + 2*C1*C2*E_Ro*RFREQ2*RL + 2*C1*C2*RFREQ1*RFREQ2*RL;
 
     return [
       [b0, b1, b2],
