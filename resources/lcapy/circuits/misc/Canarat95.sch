722c3f7387e72d112d793905d9005c7ecc33dac1
Paul Winkler <slinkp@gmail.com>
2026-02-09 09:58:49 -0500

    Finished Canarat95 circuit as drawn
    Removed vestigial R2 from big muff pickle.
    This is the circuit as redrawen 2/7/2026 by
    examining the built circuit.

--- resources/lcapy/circuits/misc/Canarat95.sch
+++ resources/lcapy/circuits/misc/Canarat95.sch
@@ -11,7 +11,6 @@ description: does this even work?
 
 RIN IN 2 10; right
 R7 2b 2mid 10e3; right
-R2 4 0a 22e3; up
 C8 2mid 0 3.3e-9; down, _
 R13 2mid 3 10e3; right
 C14 2a 4 10e-9; right
@@ -20,7 +19,5 @@ R_T 4 5 100e3; right, variable
 RL OUT 0 1e6; down
 W 2 2a; up=0.5
 W 2 2b; down=0.5
-W 0a 0b; right=0.5
-W 0b 0; down=0.25
 W 5 3; down=1.0
 W OUTa OUT; right=0.1
--- src/models/tonestacks/misc/Canarat95.js
+++ src/models/tonestacks/misc/Canarat95.js
@@ -11,7 +11,6 @@ export class Canarat95 extends BaseTonestack {
       components: {
         RIN: 10,
         R13: 10e3,
-        R2: 22e3,
         R7: 10e3,
         RT: 100e3,
         RL: 1e6,
@@ -29,17 +28,17 @@ export class Canarat95 extends BaseTonestack {
   }
 
   calculateCoefficients(controlValues) {
-    const { RIN, R13, R2, R7, RT, RL, C14, C8, C9 } = this.extractCoefficientVariables(controlValues);
+    const { RIN, R13, R7, RT, RL, C14, C8, C9 } = this.extractCoefficientVariables(controlValues);
 
     const b0 = 0;
-    const b1 = C9*R2*RL + C9*RL*RT;
-    const b2 = C14*C9*R13*R2*RL + C14*C9*R2*R7*RL + C14*C9*R2*RL*RT;
-    const b3 = C14*C8*C9*R13*R2*R7*RL;
+    const b1 = C9*RL;
+    const b2 = C14*C9*R13*RL + C14*C9*R7*RL + C14*C9*RL*RT;
+    const b3 = C14*C8*C9*R13*R7*RL;
 
-    const a0 = R13 + R2 + R7 + RIN + RT;
-    const a1 = C14*R13*R2 + C14*R13*RIN + C14*R2*R7 + C14*R2*RT + C14*R7*RIN + C14*RIN*RT + C8*R13*R7 + C8*R13*RIN + C8*R2*R7 + C8*R2*RIN + C8*R7*RT + C8*RIN*RT + C9*R13*R2 + C9*R13*RL + C9*R13*RT + C9*R2*R7 + C9*R2*RIN + C9*R2*RL + C9*R7*RL + C9*R7*RT + C9*RIN*RL + C9*RIN*RT + C9*RL*RT;
-    const a2 = C14*C8*R13*R2*R7 + C14*C8*R13*R2*RIN + C14*C8*R13*R7*RIN + C14*C8*R2*R7*RIN + C14*C8*R2*R7*RT + C14*C8*R2*RIN*RT + C14*C8*R7*RIN*RT + C14*C9*R13*R2*RIN + C14*C9*R13*R2*RL + C14*C9*R13*R2*RT + C14*C9*R13*RIN*RL + C14*C9*R13*RIN*RT + C14*C9*R2*R7*RIN + C14*C9*R2*R7*RL + C14*C9*R2*R7*RT + C14*C9*R2*RIN*RT + C14*C9*R2*RL*RT + C14*C9*R7*RIN*RL + C14*C9*R7*RIN*RT + C14*C9*RIN*RL*RT + C8*C9*R13*R2*R7 + C8*C9*R13*R2*RIN + C8*C9*R13*R7*RL + C8*C9*R13*R7*RT + C8*C9*R13*RIN*RL + C8*C9*R13*RIN*RT + C8*C9*R2*R7*RL + C8*C9*R2*RIN*RL + C8*C9*R7*RL*RT + C8*C9*RIN*RL*RT;
-    const a3 = C14*C8*C9*R13*R2*R7*RIN + C14*C8*C9*R13*R2*R7*RL + C14*C8*C9*R13*R2*R7*RT + C14*C8*C9*R13*R2*RIN*RL + C14*C8*C9*R13*R2*RIN*RT + C14*C8*C9*R13*R7*RIN*RL + C14*C8*C9*R13*R7*RIN*RT + C14*C8*C9*R2*R7*RIN*RL + C14*C8*C9*R2*R7*RL*RT + C14*C8*C9*R2*RIN*RL*RT + C14*C8*C9*R7*RIN*RL*RT;
+    const a0 = 1;
+    const a1 = C14*R13 + C14*R7 + C14*RT + C8*R7 + C8*RIN + C9*R13 + C9*R7 + C9*RIN + C9*RL;
+    const a2 = C14*C8*R13*R7 + C14*C8*R13*RIN + C14*C8*R7*RIN + C14*C8*R7*RT + C14*C8*RIN*RT + C14*C9*R13*RIN + C14*C9*R13*RL + C14*C9*R13*RT + C14*C9*R7*RIN + C14*C9*R7*RL + C14*C9*R7*RT + C14*C9*RIN*RT + C14*C9*RL*RT + C8*C9*R13*R7 + C8*C9*R13*RIN + C8*C9*R7*RL + C8*C9*RIN*RL;
+    const a3 = C14*C8*C9*R13*R7*RIN + C14*C8*C9*R13*R7*RL + C14*C8*C9*R13*R7*RT + C14*C8*C9*R13*RIN*RL + C14*C8*C9*R13*RIN*RT + C14*C8*C9*R7*RIN*RL + C14*C8*C9*R7*RL*RT + C14*C8*C9*RIN*RL*RT;
 
     return [
       [b0, b1, b2, b3],
