ea279f044b4ea0ae62ddade47edc72662cd760d7
Paul Winkler <slinkp@gmail.com>
2026-02-09 09:50:51 -0500

    Re-orient filter horizontally

--- resources/lcapy/circuits/misc/Canarat95.sch
+++ resources/lcapy/circuits/misc/Canarat95.sch
@@ -14,11 +14,12 @@ R7 2b 3 10e3; right
 R2 4 0a 22e3; up
 C8 3 0 3.3e-9; down, _
 C14 2a 4 10e-9; right
-C9 5 OUTa 22e-9; right=0.525
-RV_T 4 3 5 100e3; down
+C9 3 OUTa 22e-9; right=0.525
+R_T 4 5 100e3; right, variable
 RL OUT 0 1e6; down
 W 2 2a; up=0.5
 W 2 2b; down=0.5
 W 0a 0b; right=0.5
 W 0b 0; down=0.25
+W 5 3; down=1.0
 W OUTa OUT; right=0.1
--- src/models/tonestacks/misc/Canarat95.js
+++ src/models/tonestacks/misc/Canarat95.js
@@ -19,26 +19,28 @@ export class Canarat95 extends BaseTonestack {
         C9: 22e-9,
       },
       controls: {
-        RT: Tapers.LogA,
+        RT: {
+          taper: Tapers.LogA,
+          role: PotRole.VR,
+        },
       }
     };
   }
 
   calculateCoefficients(controlValues) {
-    const { RIN, R2, R7, RT1, RT2, RL, C14, C8, C9 } = this.extractCoefficientVariables(controlValues);
+    const { RIN, R2, R7, RT, RL, C14, C8, C9 } = this.extractCoefficientVariables(controlValues);
 
     const b0 = 0;
-    const b1 = C9*R2*RL + C9*RL*RT1;
-    const b2 = C14*C9*R2*R7*RL + C14*C9*R2*RL*RT1 + C14*C9*R2*RL*RT2;
-    const b3 = C14*C8*C9*R2*R7*RL*RT2;
+    const b1 = C9*R2*RL + C9*RL*RT;
+    const b2 = C14*C9*R2*R7*RL + C14*C9*R2*RL*RT;
 
-    const a0 = R2 + R7 + RIN + RT1 + RT2;
-    const a1 = C14*R2*R7 + C14*R2*RT1 + C14*R2*RT2 + C14*R7*RIN + C14*RIN*RT1 + C14*RIN*RT2 + C8*R2*R7 + C8*R2*RIN + C8*R7*RT1 + C8*R7*RT2 + C8*RIN*RT1 + C8*RIN*RT2 + C9*R2*R7 + C9*R2*RIN + C9*R2*RL + C9*R2*RT2 + C9*R7*RL + C9*R7*RT1 + C9*RIN*RL + C9*RIN*RT1 + C9*RL*RT1 + C9*RL*RT2 + C9*RT1*RT2;
-    const a2 = C14*C8*R2*R7*RIN + C14*C8*R2*R7*RT1 + C14*C8*R2*R7*RT2 + C14*C8*R2*RIN*RT1 + C14*C8*R2*RIN*RT2 + C14*C8*R7*RIN*RT1 + C14*C8*R7*RIN*RT2 + C14*C9*R2*R7*RIN + C14*C9*R2*R7*RL + C14*C9*R2*R7*RT1 + C14*C9*R2*RIN*RT1 + C14*C9*R2*RIN*RT2 + C14*C9*R2*RL*RT1 + C14*C9*R2*RL*RT2 + C14*C9*R2*RT1*RT2 + C14*C9*R7*RIN*RL + C14*C9*R7*RIN*RT1 + C14*C9*RIN*RL*RT1 + C14*C9*RIN*RL*RT2 + C14*C9*RIN*RT1*RT2 + C8*C9*R2*R7*RL + C8*C9*R2*R7*RT2 + C8*C9*R2*RIN*RL + C8*C9*R2*RIN*RT2 + C8*C9*R7*RL*RT1 + C8*C9*R7*RL*RT2 + C8*C9*R7*RT1*RT2 + C8*C9*RIN*RL*RT1 + C8*C9*RIN*RL*RT2 + C8*C9*RIN*RT1*RT2;
-    const a3 = C14*C8*C9*R2*R7*RIN*RL + C14*C8*C9*R2*R7*RIN*RT2 + C14*C8*C9*R2*R7*RL*RT1 + C14*C8*C9*R2*R7*RL*RT2 + C14*C8*C9*R2*R7*RT1*RT2 + C14*C8*C9*R2*RIN*RL*RT1 + C14*C8*C9*R2*RIN*RL*RT2 + C14*C8*C9*R2*RIN*RT1*RT2 + C14*C8*C9*R7*RIN*RL*RT1 + C14*C8*C9*R7*RIN*RL*RT2 + C14*C8*C9*R7*RIN*RT1*RT2;
+    const a0 = R2 + R7 + RIN + RT;
+    const a1 = C14*R2*R7 + C14*R2*RT + C14*R7*RIN + C14*RIN*RT + C8*R2*R7 + C8*R2*RIN + C8*R7*RT + C8*RIN*RT + C9*R2*R7 + C9*R2*RIN + C9*R2*RL + C9*R7*RL + C9*R7*RT + C9*RIN*RL + C9*RIN*RT + C9*RL*RT;
+    const a2 = C14*C8*R2*R7*RIN + C14*C8*R2*R7*RT + C14*C8*R2*RIN*RT + C14*C8*R7*RIN*RT + C14*C9*R2*R7*RIN + C14*C9*R2*R7*RL + C14*C9*R2*R7*RT + C14*C9*R2*RIN*RT + C14*C9*R2*RL*RT + C14*C9*R7*RIN*RL + C14*C9*R7*RIN*RT + C14*C9*RIN*RL*RT + C8*C9*R2*R7*RL + C8*C9*R2*RIN*RL + C8*C9*R7*RL*RT + C8*C9*RIN*RL*RT;
+    const a3 = C14*C8*C9*R2*R7*RIN*RL + C14*C8*C9*R2*R7*RL*RT + C14*C8*C9*R2*RIN*RL*RT + C14*C8*C9*R7*RIN*RL*RT;
 
     return [
-      [b0, b1, b2, b3],
+      [b0, b1, b2],
       [a0, a1, a2, a3]
     ];
   }
